f101a574651a6bf47241a3d58f59de76fe1b2dd1
ghazanfar <ghznfr0078@gmail.com>
2025-01-16 11:28:00 +0500

    like model

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,15 +3,58 @@ import {Like} from "../models/like.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
+    const {type} = req.query
     //TODO: toggle like on video
+    if(!isValidObjectId(videoId)) {
+        throw new ApiError(400, "video id is missing or invalid!")
+    }
+
+    const video = await Video.findById(videoId)
+
+    if(!video) {
+        throw new ApiError(402, "Video not found!")
+    }
+    const userId = req.user?._id
+
+    const query = {
+        [type]: videoId,
+        likedBy: userId,
+    };
+    const existedLike = await Like.findOne(query)
+    if(existedLike) {
+        await existedLike.deleteOne()
+        return res.status(200).json(
+            new ApiResponse(200, {}, "Like removed successfully!")
+        )
+    } else {
+        const newLike = new Like({video: video.title,
+            likedBy: userId})
+        await newLike.save({validateBeforeSave: false})
+        return res.status(200).json(
+            new ApiResponse(200, {}, "Like added successfully!")
+        )
+    }
+
+    
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
+//     if(!isValidObjectId(commentId)) {
+//         throw new ApiError(400, "Comment id is missing or Invalid!")
+//     }
+
+//     const comment = await Comment.findById(commentId)
+//     if(!comment) {
+//         throw new ApiError(402, "Comment not found!")
+//     }
+
+//     const existedLike = await
 
 })
 
